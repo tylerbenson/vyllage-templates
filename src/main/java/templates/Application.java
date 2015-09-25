3a0ca21aac12c6803379b0c9fd650948767f3cc4
uh <carlos.uh@gmail.com>
2015-09-25 15:51:21 -0300

    Working. Todo: everything else.

--- src/main/java/templates/Application.java
+++ src/main/java/templates/Application.java
@@ -1,15 +1,60 @@
 package templates;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Scanner;
+
 import templates.files.pdf.ResumeExportService;
+
+import com.lowagie.text.DocumentException;
 
 public class Application {
 
-	public static void main(String... args) throws InterruptedException {
-		ResumeExportService exportService = new ResumeExportService(null);
+	public static void main(String... args) throws InterruptedException,
+			DocumentException, IOException {
+
+		Application app = new Application();
+
+		ResumeExportService exportService = new ResumeExportService();
+
+		ByteArrayOutputStream pdfDocument = exportService
+				.generatePDFDocument(app.getFile("templates/default.html"));
+
+		OutputStream outputStream = new FileOutputStream("hello.pdf");
+		pdfDocument.writeTo(outputStream);
+
 		// Iterate through each templates/*.html file on the classpath.
 
 		// pass that in as the template to template service.
 
 		// write resulting bytes to file in build/templates
+	}
+
+	private String getFile(String fileName) {
+
+		StringBuilder result = new StringBuilder("");
+
+		// Get file from resources folder
+		ClassLoader classLoader = getClass().getClassLoader();
+		File file = new File(classLoader.getResource(fileName).getFile());
+
+		try (Scanner scanner = new Scanner(file)) {
+
+			while (scanner.hasNextLine()) {
+				String line = scanner.nextLine();
+				result.append(line).append("\n");
+			}
+
+			scanner.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return result.toString();
+
 	}
 }
--- src/main/java/templates/files/pdf/ResumeExportService.java
+++ src/main/java/templates/files/pdf/ResumeExportService.java
@@ -1,76 +1,54 @@
 package templates.files.pdf;
 
-import java.awt.Graphics2D;
-import java.awt.Image;
-import java.awt.Rectangle;
-import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.MappedByteBuffer;
-import java.nio.channels.FileChannel;
-import java.text.ParseException;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Locale;
 import java.util.logging.Logger;
-import java.util.stream.Collectors;
 
-import javax.imageio.ImageIO;
-import javax.inject.Inject;
+import javax.annotation.Resource;
 
 import org.apache.commons.lang3.exception.ExceptionUtils;
-import org.springframework.core.io.Resource;
-import org.springframework.stereotype.Service;
-import org.springframework.util.Assert;
-import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.context.Context;
 import org.xhtmlrenderer.pdf.ITextRenderer;
-import org.xhtmlrenderer.util.FSImageWriter;
 
 import com.lowagie.text.DocumentException;
-import com.newrelic.api.agent.NewRelic;
-import com.sun.pdfview.PDFFile;
-import com.sun.pdfview.PDFPage;
 
-import documents.model.DocumentHeader;
-import documents.model.document.sections.DocumentSection;
-
-@Service
+// @Service
 public class ResumeExportService {
 
 	private final Logger logger = Logger.getLogger(ResumeExportService.class
 			.getName());
 
-	private final TemplateEngine templateEngine;
-
-	@Inject
-	public ResumeExportService(final TemplateEngine templateEngine) {
-		this.templateEngine = templateEngine;
-	}
-
-	public ByteArrayOutputStream generatePDFDocument(
-			DocumentHeader resumeHeader, List<DocumentSection> sections,
-			String styleName) throws DocumentException {
+	public ByteArrayOutputStream generatePDFDocument(String string)
+			throws DocumentException {
 		ByteArrayOutputStream out = new ByteArrayOutputStream();
 
 		try {
 
-			ITextRenderer renderer = preparePDF(resumeHeader, sections,
-					styleName);
+			ITextRenderer renderer = preparePDF(string);
 			renderer.createPDF(out);
 			renderer.finishPDF();
 
 			out.close();
 		} catch (IOException e) {
 			logger.severe(ExceptionUtils.getStackTrace(e));
-			NewRelic.noticeError(e);
 		}
 
 		return out;
 
+	}
+
+	protected ITextRenderer preparePDF(String file) throws IOException {
+		ITextRenderer renderer = new ITextRenderer();
+		renderer.setDocumentFromString(file);
+		// renderer.setDocument(file);
+
+		PackageUserAgentCallback callback = new PackageUserAgentCallback(
+				renderer.getOutputDevice(), Resource.class);
+		callback.setSharedContext(renderer.getSharedContext());
+		renderer.getSharedContext().setUserAgentCallback(callback);
+		// renderer.setDocumentFromString(htmlContent);
+
+		renderer.layout();
+		return renderer;
 	}
 
 	/**
@@ -86,143 +64,87 @@ public class ResumeExportService {
 	 * @return
 	 * @throws DocumentException
 	 */
-	public ByteArrayOutputStream generatePNGDocument(
-			final DocumentHeader resumeHeader,
-			final List<DocumentSection> sections, final String styleName,
-			int width, int height) throws DocumentException {
-
-		if (width == 0 || height == 0) {
-			width = 64 * 2;
-			height = 98 * 2;
-		}
-
-		ByteArrayOutputStream out = new ByteArrayOutputStream();
-
-		ITextRenderer renderer = preparePDF(resumeHeader, sections, styleName);
-
-		File tempFile = null;
-
-		try {
-			tempFile = File.createTempFile("document", ".pdf");
-		} catch (IOException e) {
-			logger.severe(ExceptionUtils.getStackTrace(e));
-			NewRelic.noticeError(e);
-		}
-
-		Assert.notNull(tempFile);
-
-		// saving the pdf
-		try (FileOutputStream fop = new FileOutputStream(tempFile)) {
-			renderer.createPDF(fop);
-			renderer.finishPDF();
-			// if file doesn't exists, then create it
-			if (!tempFile.exists()) {
-				tempFile.createNewFile();
-			}
-
-			fop.flush();
-
-		} catch (IOException e) {
-			logger.severe(ExceptionUtils.getStackTrace(e));
-			NewRelic.noticeError(e);
-		}
-
-		// loading the pdf and rendering an image of the first page
-		try (FileInputStream fis = new FileInputStream(tempFile)) {
-			FileChannel channel = fis.getChannel();
-			MappedByteBuffer buf = channel.map(FileChannel.MapMode.READ_ONLY,
-					0, channel.size());
-			PDFFile pdffile = new PDFFile(buf);
-
-			// draw the first page to an image
-			PDFPage page = pdffile.getPage(0);
-
-			// get the width and height for the doc at the default zoom
-			Rectangle rect = new Rectangle(0, 0, (int) page.getBBox()
-					.getWidth(), (int) page.getBBox().getHeight());
-
-			BufferedImage bufferedImage = new BufferedImage(width, height,
-					BufferedImage.TYPE_INT_RGB);
-
-			Image image = page.getImage(rect.width, rect.height, // width &
-																	// height
-					rect, // clip rect
-					null, // null for the ImageObserver
-					true, // fill background with white
-					true // block until drawing is done
-					);
-			FSImageWriter w = new FSImageWriter("png");
-
-			Image scaledInstance = image.getScaledInstance(width, height,
-					Image.SCALE_SMOOTH);
-
-			Graphics2D bufImageGraphics = bufferedImage.createGraphics();
-			bufImageGraphics.drawImage(scaledInstance, 0, 0, null);
-
-			ImageIO.write(bufferedImage, "png", out);
-			w.write(bufferedImage, out);
-			tempFile.delete();
-
-			out.close();
-
-		} catch (IOException e) {
-			logger.severe(ExceptionUtils.getStackTrace(e));
-			NewRelic.noticeError(e);
-		}
-
-		return out;
-
-	}
-
-	protected ITextRenderer preparePDF(DocumentHeader resumeHeader,
-			List<DocumentSection> sections, String styleName) {
-		Context ctx = new Context();
-
-		format(resumeHeader);
-
-		ctx.setVariable("styleName", styleName);
-
-		ctx.setVariable("header", resumeHeader);
-
-		ctx.setVariable("sections", sections.stream().sorted(sortSections())
-				.collect(Collectors.toList()));
-
-		String htmlContent = templateEngine.process("pdf-resume", ctx);
-
-		ITextRenderer renderer = new ITextRenderer();
-		renderer.setDocumentFromString(htmlContent);
-
-		PackageUserAgentCallback callback = new PackageUserAgentCallback(
-				renderer.getOutputDevice(), Resource.class);
-		callback.setSharedContext(renderer.getSharedContext());
-		renderer.getSharedContext().setUserAgentCallback(callback);
-		renderer.setDocumentFromString(htmlContent);
-
-		renderer.layout();
-		return renderer;
-	}
-
-	/**
-	 * Applies formatting to header phone number in Locale.US.
-	 *
-	 * @param resumeHeader
-	 * @throws ParseException
-	 */
-	protected void format(DocumentHeader resumeHeader) {
-		PhoneNumberFormatter formatter = new PhoneNumberFormatter();
-
-		resumeHeader.setPhoneNumber(formatter.print(
-				resumeHeader.getPhoneNumber(), Locale.US));
-	}
-
-	/**
-	 * Sorts sections based on their sectionPosition value.
-	 *
-	 * @return sorted sections
-	 */
-	protected Comparator<? super DocumentSection> sortSections() {
-		return (s1, s2) -> s1.getSectionPosition().compareTo(
-				s2.getSectionPosition());
-	}
+	// public ByteArrayOutputStream generatePNGDocument(
+	// final DocumentHeader resumeHeader,
+	// final List<DocumentSection> sections, final String styleName,
+	// int width, int height) throws DocumentException {
+	//
+	// if (width == 0 || height == 0) {
+	// width = 64 * 2;
+	// height = 98 * 2;
+	// }
+	//
+	// ByteArrayOutputStream out = new ByteArrayOutputStream();
+	//
+	// ITextRenderer renderer = preparePDF(resumeHeader, sections, styleName);
+	//
+	// File tempFile = null;
+	//
+	// try {
+	// tempFile = File.createTempFile("document", ".pdf");
+	// } catch (IOException e) {
+	// logger.severe(ExceptionUtils.getStackTrace(e));
+	// }
+	//
+	// // saving the pdf
+	// try (FileOutputStream fop = new FileOutputStream(tempFile)) {
+	// renderer.createPDF(fop);
+	// renderer.finishPDF();
+	// // if file doesn't exists, then create it
+	// if (!tempFile.exists()) {
+	// tempFile.createNewFile();
+	// }
+	//
+	// fop.flush();
+	//
+	// } catch (IOException e) {
+	// logger.severe(ExceptionUtils.getStackTrace(e));
+	// }
+	//
+	// // loading the pdf and rendering an image of the first page
+	// try (FileInputStream fis = new FileInputStream(tempFile)) {
+	// FileChannel channel = fis.getChannel();
+	// MappedByteBuffer buf = channel.map(FileChannel.MapMode.READ_ONLY,
+	// 0, channel.size());
+	// PDFFile pdffile = new PDFFile(buf);
+	//
+	// // draw the first page to an image
+	// PDFPage page = pdffile.getPage(0);
+	//
+	// // get the width and height for the doc at the default zoom
+	// Rectangle rect = new Rectangle(0, 0, (int) page.getBBox()
+	// .getWidth(), (int) page.getBBox().getHeight());
+	//
+	// BufferedImage bufferedImage = new BufferedImage(width, height,
+	// BufferedImage.TYPE_INT_RGB);
+	//
+	// Image image = page.getImage(rect.width, rect.height, // width &
+	// // height
+	// rect, // clip rect
+	// null, // null for the ImageObserver
+	// true, // fill background with white
+	// true // block until drawing is done
+	// );
+	// FSImageWriter w = new FSImageWriter("png");
+	//
+	// Image scaledInstance = image.getScaledInstance(width, height,
+	// Image.SCALE_SMOOTH);
+	//
+	// Graphics2D bufImageGraphics = bufferedImage.createGraphics();
+	// bufImageGraphics.drawImage(scaledInstance, 0, 0, null);
+	//
+	// ImageIO.write(bufferedImage, "png", out);
+	// w.write(bufferedImage, out);
+	// tempFile.delete();
+	//
+	// out.close();
+	//
+	// } catch (IOException e) {
+	// logger.severe(ExceptionUtils.getStackTrace(e));
+	// }
+	//
+	// return out;
+	//
+	// }
 
 }
