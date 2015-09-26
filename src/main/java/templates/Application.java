38ee0d1b5e17d6396ec6dd3b978a6f3ef43096e3
uh <carlos.uh@gmail.com>
2015-09-25 22:29:17 -0300

    Revert "Revert "Revert "Loop and print."""
    This reverts commit c56a7a226fea70da4281524f7c0799d4ca3d7dae.

--- src/main/java/templates/Application.java
+++ src/main/java/templates/Application.java
@@ -5,18 +5,13 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.logging.Logger;
+import java.util.Scanner;
 
 import templates.files.pdf.ResumeExportService;
 
 import com.lowagie.text.DocumentException;
 
 public class Application {
-
-	private static Logger logger = Logger
-			.getLogger(Application.class.getName());
 
 	public static void main(String... args) throws InterruptedException,
 			DocumentException, IOException {
@@ -25,38 +20,41 @@ public class Application {
 
 		ResumeExportService exportService = new ResumeExportService();
 
+		ByteArrayOutputStream pdfDocument = exportService
+				.generatePDFDocument(app.getFile("templates/default.html"));
+
+		OutputStream outputStream = new FileOutputStream("hello.pdf");
+		pdfDocument.writeTo(outputStream);
+
 		// Iterate through each templates/*.html file on the classpath.
-		for (File file : app.getTemplates()) {
 
-			// pass that in as the template to template service.
-			ByteArrayOutputStream pdfDocument = exportService
-					.generatePDFDocument(file);
+		// pass that in as the template to template service.
 
-			OutputStream outputStream = new FileOutputStream(file.getName()
-					.replace("html", "pdf"));
-
-			// write resulting bytes to file in build/templates
-			pdfDocument.writeTo(outputStream);
-		}
-
+		// write resulting bytes to file in build/templates
 	}
 
-	private List<File> getTemplates() {
-		List<File> templates = new ArrayList<>();
-		File templateDirectory = this.getTemplateDirectory();
+	private String getFile(String fileName) {
 
-		for (File file : templateDirectory.listFiles()) {
-			if (file.isFile() && file.getName().contains("html")) {
-				logger.info("Preparing to print: " + file.getAbsolutePath());
-				templates.add(new File(file.getAbsolutePath()));
-			}
-		}
-		return templates;
-	}
+		StringBuilder result = new StringBuilder("");
 
-	private File getTemplateDirectory() {
+		// Get file from resources folder
 		ClassLoader classLoader = getClass().getClassLoader();
-		return new File(classLoader.getResource("templates/").getFile());
-	}
+		File file = new File(classLoader.getResource(fileName).getFile());
 
+		try (Scanner scanner = new Scanner(file)) {
+
+			while (scanner.hasNextLine()) {
+				String line = scanner.nextLine();
+				result.append(line).append("\n");
+			}
+
+			scanner.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return result.toString();
+
+	}
 }
